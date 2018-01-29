7b9bcc93bbbb2d5b912475cb528d49932aef090d
zxcv1246789 <zxcv1246789@naver.com>
2018-01-29 15:31:38 +0900

    test

--- router/main.js
+++ router/main.js
@@ -5,7 +5,10 @@ module.exports = function(app, fs, url) {
   });
 
   app.get('/download', function(req, res) {
-    var file = __dirname + '/../package/dhcpserver.zip';
+    var name = req.query.name;
+
+    var file = __dirname + '/../package/' + name + '.zip';
+
     res.download(file); // Set disposition and send it.
   });
 }
