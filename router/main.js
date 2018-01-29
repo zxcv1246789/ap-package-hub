f28884dd6ae32e1cf85a85ca10717fbb64af2e62
zxcv1246789 <zxcv1246789@naver.com>
2018-01-29 15:39:47 +0900

    test

--- router/main.js
+++ router/main.js
@@ -1,5 +1,5 @@
 module.exports = function(app, fs, url) {
-  var router_system = require('./download/download.js');
+  var router_download = require('./download/download.js');
 
   app.get('/', function(req, res) {
     res.render('index.html');
@@ -13,10 +13,6 @@ module.exports = function(app, fs, url) {
     res.download(file); // Set disposition and send it.
   });
   app.get('/package', function(req, res) {
-    var name = req.query.name;
-
-    var file = __dirname + '/../package/' + name + '.zip';
-
-    res.download(file); // Set disposition and send it.
+     router_download.ap_server_package(req, res);
   });
 }
