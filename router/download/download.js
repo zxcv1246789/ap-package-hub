9becc93adf2d6097c99b61dc606631b4c98fe1b3
zxcv1246789 <zxcv1246789@naver.com>
2018-01-29 15:38:30 +0900

    test

--- router/download/download.js
+++ router/download/download.js
@@ -5,6 +5,16 @@ const {
   execSync
 } = require('child_process');
 
-exports.ap_server_package = function(req, res) {}
-
+exports.ap_server_package = function(req, res) {
+  var files = fs.readdirSync(__dirname + '/../../package/');
+  console.log(files.length);
+  var sidemenus = {};
+  for (var i = 0; i < files.length; i++) {
+    var dir_name = files[i];
+    var sidemenu = {};
+    var dd = "package _" + String(i + 1);
+    sidemenu['pack_name'] = dir_name.replace('.zip', '');
+    sidemenus[dd] = sidemenu;
+  }
+  res.send(sidemenus);
 }
--- router/main.js
+++ router/main.js
@@ -1,10 +1,18 @@
 module.exports = function(app, fs, url) {
+  var router_system = require('./download/download.js');
 
   app.get('/', function(req, res) {
     res.render('index.html');
   });
 
   app.get('/download', function(req, res) {
+    var name = req.query.name;
+
+    var file = __dirname + '/../package/' + name + '.zip';
+
+    res.download(file); // Set disposition and send it.
+  });
+  app.get('/package', function(req, res) {
     var name = req.query.name;
 
     var file = __dirname + '/../package/' + name + '.zip';
