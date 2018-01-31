4a84c773bdbe387c73898a7ac8104388efe607c7
zxcv1246789 <zxcv1246789@naver.com>
2018-01-31 09:03:05 +0900

    test

--- router/download/download.js
+++ router/download/download.js
@@ -13,11 +13,10 @@ exports.ap_server_package = function(req, res) {
     var dir_name = files[i];
     var sidemenu = {};
     var dd = "package _" + String(i + 1);
-    if (dir_name.indexOf(".md5") != -1) {
-
-    } else {
+    if (dir_name.indexOf(".zip") != -1) {
       sidemenu['pack_name'] = dir_name.replace('.zip', '');
       sidemenus[dd] = sidemenu;
+    } else {
     }
   }
   res.send(sidemenus);
