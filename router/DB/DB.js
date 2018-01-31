7e50bd187c31f01ca64e61328619ae6ba67acb80
zxcv1246789 <zxcv1246789@naver.com>
2018-01-31 14:43:14 +0900

    test

--- router/DB/DB.js
+++ router/DB/DB.js
@@ -7,33 +7,34 @@ const {
 
 var mysql = require('mysql');
 var pool = mysql.createPool({
-    connectionLimit: 10,
-    host     : 'localhost',
-    user     : 'root',
-    password : 'raspberry',
-    database : 'aphubdb'
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  password: 'raspberry',
+  database: 'aphubdb'
 });
 
 exports.insert_download_log = function(req, res, username, type, packagename) {
   var content = "";
-  if (type == 1){
+  if (type == 1) {
     content += packagename + " download 완료";
   }
   var data = [username, content];
 
-  pool.getConnection(function (err, connection) {
+  pool.getConnection(function(err, connection) {
     var sql = "";
     sql = "INSERT INTO pkg_download_history(Name, Date, Log_content) VALUES(?, NOW(),?);";
-    connection.query(sql, data, function (err, rows) {
-      if(err) console.error("err : " + err);
+    if (err) throw err;
+
+    connection.query(sql, data, function(err, rows) {
+
       console.log("확인");
-      res.redirect('/');
-      connection.release();
     });
+    connection.release();
   });
 
   result = {
-    'success' : '1'
+    'success': '1'
   }
   res.send(result);
 }
