d22c07e1088b6094646e23dbde3102fd460e6eb1
zxcv1246789 <zxcv1246789@naver.com>
2018-02-21 15:58:17 +0900

    test

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ var server = app.listen(80, function() {
 });
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'Vue')));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
