09a693ab07fd421968a19c5c849c034385a513f1
zxcv1246789 <zxcv1246789@naver.com>
2018-02-26 15:06:25 +0900

    test

--- frontend/src/components/ShowPage.vue
+++ frontend/src/components/ShowPage.vue
@@ -1,20 +1,20 @@
 <template>
-  <div>
-    <h1>상세 내용</h1>
+<div>
+  <h1>상세 내용</h1>
 
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  created: function () {
+  created: function() {
     var id = this.$route.params.id
     this.$http.get(`/api/movies/${id}`)
       .then((response) => {
         this.movie = response.data
       })
   },
-  data: function () {
+  data: function() {
     return {
       movie: {}
     }
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,8 +7,7 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'index',
       component: Index
