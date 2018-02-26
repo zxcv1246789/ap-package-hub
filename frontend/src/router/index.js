dd7bdff545ecaf48c6933dcc50d202faab99a1ef
zxcv1246789 <zxcv1246789@naver.com>
2018-02-26 13:51:42 +0900

    test

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
+import Index from 'components/IndexPage'
+import Show from 'components/ShowPage'
+
 export default new Router({
+//  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'index',
+      component: Index
+    },
+    {
+      path: '/:id',
+      name: 'show',
+      component: Show
     }
   ]
 })
