6b5c5f196197fc8e10419bf13c88395c4d1546c7
zxcv1246789 <zxcv1246789@naver.com>
2018-02-26 11:02:27 +0900

    test

--- Vue/test.js
+++ Vue/test.js
@@ -2,14 +2,8 @@ var app = new Vue({
   el: '#app',
 
   data: {
-    name: 'Vue'
+    name: 'Vue',
+    file: null,
+    file2: null
   }
 })
-export default {
-  data () {
-    return {
-      file: null,
-      file2: null
-    }
-  }
-}
