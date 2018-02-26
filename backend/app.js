be066afbf092408573c2ac0c3b7bdf4acd95e72c
zxcv1246789 <zxcv1246789@naver.com>
2018-02-26 17:05:27 +0900

    test

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
 var app = express();
 
 app.use(require('connect-history-api-fallback')())
@@ -21,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,4 +40,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-require('./routes/main.js')(app);
+
+require('./router/main.js')(app);
+
+module.exports = app;
