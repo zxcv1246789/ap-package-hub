8fe027fe39b3b06ac7708bae50f1ce01e47ffafd
zxcv1246789 <zxcv1246789@naver.com>
2018-02-26 15:18:56 +0900

    test

--- frontend/src/components/IndexPage.vue
+++ frontend/src/components/IndexPage.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="movies">
-    <h1>영화 목록123</h1>
-    <div v-for="movie in movies" class="movie">
-      <img v-bind:src="movie.poster" class="poster">
-      <div>
-        <strong></strong>, <i></i> []
-        <router-link :to="{ name: 'show', params: { id: movie.id }}">더보기</router-link>
-      </div>
+<div class="movies">
+  <h1>영화 목록123</h1>
+  <div v-for="movie in movies" class="movie">
+    <img v-bind:src="movie.poster" class="poster">
+    <div>
+      <strong></strong>, <i></i> []
+      <router-link :to="{ name: 'show', params: { id: movie.id }}">더보기</router-link>
     </div>
   </div>
+</div>
 </template>
 
 <script>
 export default {
-  created () {
+  created() {
     this.$http.get('/api/movies')
-    .then((response) => {
-      this.movies = response.data
-      console.log(this.movies);
-    })
+      .then((response) => {
+        this.movies = response.data
+        console.log(this.movies);
+      })
   },
-  data () {
+  data() {
     return {
       movies: []
     }
