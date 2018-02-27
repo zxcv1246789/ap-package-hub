06d155a9893168602c5ee1a576de50c41f9c72fb
zxcv1246789 <zxcv1246789@naver.com>
2018-02-27 18:03:17 +0900

    test

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,23 +2,50 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 //import에서 .vue 생략
 import Vue from 'vue'
-import VueRouters from 'vue-router'
+import VueRouter from 'vue-router'
 import axios from 'axios'
 import BootstrapVue from 'bootstrap-vue'
-import routes from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
+import Upload from './components/Upload'
+import Table from './components/Table'
 
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(BootstrapVue);
-Vue.use(VueRouters);
+Vue.use(VueRouter);
+
+/*
+export default new VueRouter({
+  mode: 'history',
+  routes: [{
+    path: '/upload',
+    component: Upload
+  },
+  {
+    path: '/table',
+    component: Table
+  }
+  ]
+})*/
+const routes = [
+  {
+    path: '/upload',
+    component: Upload
+  },
+  {
+    path: '/table',
+    component: Table
+  }
+];
+
 
 /* eslint-disable no-new */
-const router = new VueRouters({
+const router = new VueRouter({
   routes // routes: routes 의 약어
 })
 
 const app = new Vue({
+  el: '#app',
   router
-}).$mount('#app')
+});
