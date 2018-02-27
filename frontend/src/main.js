6c2dc34ad279a937931570e182afb4aa2581ad69
zxcv1246789 <zxcv1246789@naver.com>
2018-02-27 17:53:42 +0900

    test

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 //import에서 .vue 생략
-import Vue from 'vue'
+import Vue from 'vue';
 import VueRouters from 'vue-router'
 import axios from 'axios'
 import BootstrapVue from 'bootstrap-vue'
