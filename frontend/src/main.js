9f2c66e1cf867aa167ebe12eddfdae6611d3f51a
zxcv1246789 <zxcv1246789@naver.com>
2018-02-27 14:31:36 +0900

    test

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,15 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 //import에서 .vue 생략
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import Upload from './components/Upload'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
-//전역 컴포넌트 등록
 Vue.use(BootstrapVue);
+
+//전역 컴포넌트 등록
+Vue.component('app-upload', Upload);
 
 /* eslint-disable no-new */
 new Vue({
