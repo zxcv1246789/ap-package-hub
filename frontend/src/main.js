cd4a648969fd5e29db5196dbf51dce892749db69
zxcv1246789 <zxcv1246789@naver.com>
2018-02-27 17:55:50 +0900

    test

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 //import에서 .vue 생략
-import Vue from 'vue';
+import Vue from 'vue'
 import VueRouters from 'vue-router'
 import axios from 'axios'
 import BootstrapVue from 'bootstrap-vue'
@@ -12,6 +12,7 @@ import "bootstrap-vue/dist/bootstrap-vue.css"
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(BootstrapVue);
+Vue.use(VueRouter);
 
 /* eslint-disable no-new */
 const router = new VueRouters({
