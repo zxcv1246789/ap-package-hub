98c58c3e30c1d2db4eedac140125b1ea263d52fa
zxcv1246789 <zxcv1246789@naver.com>
2018-02-27 10:36:58 +0900

    test

--- frontend/src/components/Upload.vue
+++ frontend/src/components/Upload.vue
@@ -3,15 +3,25 @@
   <!-- Styled -->
   <b-form-file v-model="file" :state="Boolean(file)"></b-form-file>
   <div class="mt-3">Selected file: {{file && file.name}}</div>
-
+  <h1>카운터: {{ number }}</h1>
+  <button @click="increment">증가</button>
+  <button @click="decrement">감소</button>
 </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       file: null
+    }
+  },
+  methods: {
+    increment: function() {
+      this.number++;
+    },
+    decrement: function() {
+      this.number--;
     }
   }
 }
