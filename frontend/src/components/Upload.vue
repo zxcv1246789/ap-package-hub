1f6a571808f8c4707b96573d4ee26ca2e30833ae
zxcv1246789 <zxcv1246789@naver.com>
2018-02-27 15:05:06 +0900

    test

--- frontend/src/components/Upload.vue
+++ frontend/src/components/Upload.vue
@@ -18,26 +18,30 @@ export default {
   },
   methods: {
     submitFile() {
-      console.log(this.file);
-              //Initialize the form data
-      let formData = new FormData();
-          //Add the form data we need to submit
-      formData.append('package', this.file);
+      if (this.file != null) {
+        console.log(this.file);
+        //Initialize the form data
+        let formData = new FormData();
+        //Add the form data we need to submit
+        formData.append('package', this.file);
         //Make the request to the POST /single-file URL
-      axios.post('http://39.119.118.152:3000/api/upload',
-          formData, {
-            headers: {
-              'Content-Type': 'multipart/form-data'
+        axios.post('http://39.119.118.152:3000/api/upload',
+            formData, {
+              headers: {
+                'Content-Type': 'multipart/form-data'
+              }
             }
-          }
-        ).then(function() {
-          console.log('SUCCESS!!');
-        })
-        .catch(function() {
-          console.log('FAILURE!!');
-        });
+          ).then(function() {
+            console.log('SUCCESS!!');
+          })
+          .catch(function() {
+            console.log('FAILURE!!');
+          });
+      }else {
+        alert("파일을 선택해주세요.");
+      }
     },
-      //Handles a change on the file upload
+    //Handles a change on the file upload
     handleFileUpload() {
       this.file = this.$refs.file.files[0];
     }
