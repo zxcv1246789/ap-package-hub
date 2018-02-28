4bc377b5f8558a6f0661195ba7dc6e33444f1461
zxcv1246789 <zxcv1246789@naver.com>
2018-02-28 11:30:38 +0900

    test

--- frontend/src/components/Packagelist.vue
+++ frontend/src/components/Packagelist.vue
@@ -12,11 +12,12 @@ export default {
     axios.get('http://39.119.118.152:3000/api/package')
       .then((response) => {
         console.log(response.data);
+        var tmp = new Array();
         var install_data_key = Object.getOwnPropertyNames(response.data);
         for (var j = 0; j < Object.keys(response.data).length; j++) {
-          this.items.push(response.data[install_data_key[j]]);
+          tmp.push(response.data[install_data_key[j]]);
         }
-        console.log(this.items);
+        this.items = tmp;
       })
   },
   data () {
