edcac1a0291673861357d0b23623d474ec26c95c
zxcv1246789 <zxcv1246789@naver.com>
2018-02-28 11:26:59 +0900

    test

--- frontend/src/components/Packagelist.vue
+++ frontend/src/components/Packagelist.vue
@@ -12,6 +12,11 @@ export default {
     axios.get('http://39.119.118.152:3000/api/package')
       .then((response) => {
         console.log(response.data);
+        var install_data_key = Object.getOwnPropertyNames(response.data);
+        for (var j = 0; j < Object.keys(response.data).length; j++) {
+          this.items.push(response.data[install_data_key[j]]);
+        }
+        console.log(this.items);
       })
   },
   data () {
