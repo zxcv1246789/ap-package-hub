a8b4da0db51be657af2a658feea405664d353149
zxcv1246789 <zxcv1246789@naver.com>
2018-02-28 16:12:29 +0900

    test

---
+++ frontend/src/components/Logout.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <b-button v-on:click="submitlogout()">로그아웃</b-button>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      id: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitlogin() {
+      let data = new Object();
+      data.id = this.id;
+      data.password = this.password;
+
+      axios.get('http://39.119.118.152:3000/logout')
+        .then((response) => {
+          alert("로그아웃 되었습니다.");
+        })
+
+
+    }
+  }
+}
+</script>
