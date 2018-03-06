56c1af2bbb99cc34a73a135b79fc3dcb9721cf5e
zxcv1246789 <zxcv1246789@naver.com>
2018-03-06 17:33:24 +0900

    test

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,9 @@ var userid = "";
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+var index = require('./routes/index.js')(app, isAuthenticated, passport);
+var main = require('./routes/main.js')(app, isAuthenticated, passport);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -33,6 +36,9 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use('/', index);
+app.use('/api', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -87,9 +93,5 @@ passport.use('local-login', new LocalStrategy({
     return done(false, null)
   }
 }))
-
-
-require('./routes/index.js')(app, isAuthenticated, passport);
-require('./routes/main.js')(app, isAuthenticated, passport);
 
 module.exports = app;
