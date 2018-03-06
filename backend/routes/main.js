a14a1a5c6c159750e45b2b98d89ebfdaf9c75068
zxcv1246789 <zxcv1246789@naver.com>
2018-03-06 17:24:00 +0900

    test

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -4,27 +4,27 @@ module.exports = function(app, isAuthenticated, passport) {
   var router_hash = require('./hash/hash.js');
   var router_DB = require('./DB/DB.js');
 
-  app.get('/download', isAuthenticated, function(req, res) {
+  app.get('/api/download', isAuthenticated, function(req, res) {
     var name = req.query.name;
     const file = router_download.download_package();
     res.download(file);
   });
-  app.get('/hash', isAuthenticated, function(req, res) {
+  app.get('/api/hash', isAuthenticated, function(req, res) {
     var name = req.query.name;
     const file = router_download.download_hash();
     res.download(file);
   });
 
-  app.get('/package', isAuthenticated, function(req, res) {
+  app.get('/api/package', isAuthenticated, function(req, res) {
     const sidemenus = router_download.ap_server_package();
     res.send(sidemenus);
   });
 
-  app.post('/upload', isAuthenticated, function(req, res) {
+  app.post('/api/upload', isAuthenticated, function(req, res) {
     router_upload.upload_package(req, res);
   });
 
-  app.get('/savelog', isAuthenticated, function(req, res) {
+  app.get('/api/savelog', isAuthenticated, function(req, res) {
     var username = req.query.username;
     var type = req.query.type;
     var packagename = req.query.packagename;
@@ -35,19 +35,19 @@ module.exports = function(app, isAuthenticated, passport) {
     res.send(result);
   });
 
-  app.get('/download_history', isAuthenticated, function(req, res) {
+  app.get('/api/download_history', isAuthenticated, function(req, res) {
     router_DB.download_history_get(res);
   });
 
-  app.get('/upload_history', isAuthenticated, function(req, res) {
+  app.get('/api/upload_history', isAuthenticated, function(req, res) {
     router_DB.upload_history_get(res);
   });
 
-  app.get('/download_history_array', isAuthenticated, function(req, res) {
+  app.get('/api/download_history_array', isAuthenticated, function(req, res) {
     router_DB.download_history_array(res);
   });
 
-  app.get('/upload_history_array', isAuthenticated, function(req, res) {
+  app.get('/api/upload_history_array', isAuthenticated, function(req, res) {
     router_DB.upload_history_array(res);
   });
 }
