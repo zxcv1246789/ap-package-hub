31e15194f1b9c068371d58dd72cd237b603436a8
zxcv1246789 <zxcv1246789@naver.com>
2018-03-06 17:43:18 +0900

    test

--- backend/app.js
+++ backend/app.js
@@ -7,37 +7,35 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+
+
+var index = require('./routes/index');
+var main = require('./routes/main');
+
 var app = express();
-var userid = "";
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-var index = require('./routes/index.js');
-var main = require('./routes/main.js');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'LeeJinWoo',
   saveUninitialized: true,
   resave: false,
-  cookie: {
-    maxAge: 600000
-  } //600000 = 10분
+  cookie: { maxAge: 600000 }//600000 = 10분
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', index);
+app.use('/api/movies', movies);
 app.use('/api', main);
 
 // catch 404 and forward to error handler
@@ -67,17 +65,9 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(user, done) {
   console.log('deserializeUser() 호출됨.');
   console.dir(user);
-  console.log("userid = " + userid);
+
   done(null, user);
 });
-
-var isAuthenticated = function(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  } else {
-    res.redirect('/');
-  }
-};
 
 passport.use('local-login', new LocalStrategy({
   usernameField: 'id',
@@ -85,7 +75,6 @@ passport.use('local-login', new LocalStrategy({
   passReqToCallback: true
 }, function(req, id, password, done) {
   if (id == 'admin' && password == '12341234') {
-    userid = id;
     return done(null, {
       'user_id': id,
     });
@@ -93,10 +82,6 @@ passport.use('local-login', new LocalStrategy({
     return done(false, null)
   }
 }))
-
-
-index(app, isAuthenticated, passport)
-main(app, isAuthenticated, passport)
 
 
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,37 +1,45 @@
-module.exports = function(app, isAuthenticated, passport) {
-  var path = require('path');
-  var router_img = require('./img/img.js');
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+var router_img = require('./img/img.js');
+var passport = require('passport');
 
+var isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/loginfail');
+};
 
-  app.post('/login_check', passport.authenticate('local-login', {
-      failureRedirect: '/loginfail',
-      failureFlash: true
-    }),
-    function(req, res) {
-      res.redirect('/loginsuccess');
-    });
-
-  app.get('/logout', isAuthenticated, function(req, res) {
-    console.log("로그아웃 확인");
-    req.logout();
-    res.redirect('/loginfail');
+router.post('/login_check', passport.authenticate('local-login', {
+    failureRedirect: '/loginfail',
+    failureFlash: true
+  }),
+  function(req, res) {
+    res.redirect('/loginsuccess');
   });
 
-  app.get('/', function (req, res, next) {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
-  });
+router.get('/logout', isAuthenticated, function(req, res) {
+  console.log("로그아웃 확인");
+  req.logout();
+  res.redirect('/loginfail');
+});
 
-  app.get('/loginfail', function (req, res, next) {
-    var data = false;
-    res.send(data);
-  });
-  app.get('/loginsuccess', function (req, res, next) {
-    var data = true;
-    res.send(data);
-  });
+router.get('/', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
 
-  app.get('/favicon.ico', function(req, res) {
-    const favicon = router_img.get_favicon();
-    res.send(favicon);
-  });
-}
+router.get('/loginfail', function (req, res, next) {
+  var data = false;
+  res.send(data);
+});
+router.get('/loginsuccess', function (req, res, next) {
+  var data = true;
+  res.send(data);
+});
+
+router.get('/favicon.ico', function(req, res) {
+  const favicon = router_img.get_favicon();
+  res.send(favicon);
+});
+
+module.exports = router;
