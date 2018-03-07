d9dd9d880267e40ebe45cf566769c1217f1bc7f4
zxcv1246789 <zxcv1246789@naver.com>
2018-03-07 17:14:40 +0900

    test

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,9 @@ var facebooklogin = require('./routes/facebooklogin');
 
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Max-Age", "3600");
+    res.header("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
     next();
 });
 
