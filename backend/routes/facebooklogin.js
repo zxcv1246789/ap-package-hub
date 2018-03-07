1e17829d60cebacee14b1ad1badf2cf594ee49d5
zxcv1246789 <zxcv1246789@naver.com>
2018-03-07 13:18:54 +0900

    test

--- backend/routes/facebooklogin.js
+++ backend/routes/facebooklogin.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var cors = require('cors');
 
-
-router.get('/auth/facebook', passport.authenticate('facebook', {
+router.get('/auth/facebook', cors(), passport.authenticate('facebook', {
   authType: 'rerequest',
   scope: ['public_profile', 'email']
 }));
-router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+router.get('/auth/facebook/callback', cors(), passport.authenticate('facebook', {
   failureRedirect: '/loginfail'
 }), function(req, res) {
   res.redirect('/loginsuccess');
