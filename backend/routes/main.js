364a1b7ea9d88e8934384926916f100948bb7d58
zxcv1246789 <zxcv1246789@naver.com>
2018-03-07 09:22:18 +0900

    test

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -7,7 +7,9 @@ var router_DB = require('./DB/DB.js');
 var router = express.Router();
 
 var isAuthenticated = function (req, res, next) {
-  //console.log(req.session.passport.user);
+  if(req.session.passport.user != null) {
+      console.log("user = " + req.session.passport.user);
+  }
   if (req.isAuthenticated())
     return next();
   res.redirect('/loginfail');
