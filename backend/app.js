d4636649187fd6106f9faaaa1473af35bb065f50
zxcv1246789 <zxcv1246789@naver.com>
2018-03-07 13:15:17 +0900

    test

--- backend/app.js
+++ backend/app.js
@@ -20,13 +20,14 @@ var facebooklogin = require('./routes/facebooklogin');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.options('*', cors())
+
 var server = app.listen(3000, function() {
   console.log("RaspAP server has started on port 3000");
 });
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
