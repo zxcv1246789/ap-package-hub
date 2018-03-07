dc96f872c3fbffbf65eb3aef6e309a9dd00d11e3
zxcv1246789 <zxcv1246789@naver.com>
2018-03-07 12:49:16 +0900

    test

--- frontend/src/components/Userlogin.vue
+++ frontend/src/components/Userlogin.vue
@@ -20,15 +20,17 @@ export default {
       data.id = this.id;
       data.password = this.password;
 
-      axios.get('http://39.119.118.152:3000/api/auth/facebook', { crossdomain: true })
-          .then((response) => {
+      axios.get('http://39.119.118.152:3000/api/auth/facebook', headers: {
+          "Access-Control-Allow-Origin": "*"
+        })
+        .then((response) => {
 
-          })
-        },
-        //Handles a change on the file upload
-        handleFileUpload() {
-          //this.file = this.$refs.file.files[0];
-        }
+        })
+    },
+    //Handles a change on the file upload
+    handleFileUpload() {
+      //this.file = this.$refs.file.files[0];
     }
   }
+}
 </script>
