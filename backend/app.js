77dd1f5bfbc4dd77b31c9eba4a5b6641c3349bcf
zxcv1246789 <zxcv1246789@naver.com>
2018-03-15 18:09:32 +0900

    test

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,49 @@ var server = app.listen(3000, function() {
 });
 
 app.use(cors());
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(session({
+  secret: 'LeeJinWoo',
+  saveUninitialized: true,
+  resave: false,
+  cookie: {
+    maxAge: 600000
+  } //600000 = 10분
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+});
+
 
 passport.serializeUser(function(user, done) {
   console.log('serializeUser() 호출됨.');
@@ -80,47 +123,6 @@ passport.use(new FacebookStrategy({
   });
 }));*/
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  secret: 'LeeJinWoo',
-  saveUninitialized: true,
-  resave: false,
-  cookie: {
-    maxAge: 600000
-  } //600000 = 10분
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-});
 
 var isAuthenticated = function(req, res, next) {
   if (req.isAuthenticated())
